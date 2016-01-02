9df14abcfa5289fc9fa26c8793dfd42fc69e7f43
Ali Khan <ali.9raz@gmail.com>
2016-01-03 04:38:39 +0500

    Hahahaha

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $("#drama").click(function(){
+    $(".panel").click(function(){
+        alert($(this).data("genre") + " has been added to your interests");
         $(this).fadeOut(500);
     });
 })
